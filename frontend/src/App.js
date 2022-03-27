871044e8000da68793a71f90d6364bfb52a69148
yusdalismaria <yusdalismmg@gmail.com>
2022-03-27 09:52:34 -0400

    front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,28 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
-    return ( <
-        >
-        <
-        Router >
-        <
-        div className = 'container' >
-        <
-        Header / >
+    return(
+        <>
+        <Router>
+        <div className='container'>
+        <Header/>
 
-        <
-        Routes >
-        <
-        Route path = '/'
-        element = { < Dashboard / > }
-        /> <
-        Route path = '/login'
-        element = { < Login / > }
-        /> <
-        Route path = '/register'
-        element = { < Register / > }
-        /> <
-        /Routes> <
-        /div>
+        <Routes>
+            <Route path='/' element={<Dashboard/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/register' element={<Register/>}/>
+        </Routes>
+        </div>
 
-        <
-        /Router> <
-        />
+        </Router>
+        <ToastContainer/>
+        </>
     )
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import {useSelector,useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
 import {useState, useEffect} from 'react'
 import {FaUser} from 'react-icons/fa'
+import {register,reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Register() {
   const [formData,setFormData]=useState({
@@ -11,7 +16,28 @@ function Register() {
     role:''
   })
 
+
   const {name,email,password,password2,role}=formData;
+  const navigate = useNavigate();
+  const dispatch= useDispatch();
+
+  const {user,isLoading, isError,isSuccess,message}=useSelector((state)=>state.auth
+  )
+
+
+  useEffect(()=>{
+    if(isError){
+      toast.error(message)
+    }
+    if(isSuccess || user){
+      navigate('/');
+    }
+
+    dispatch(reset());
+
+  }, [user,isError,isSuccess,isLoading,message,navigate,dispatch])
+
+
 
   const onChange=(e)=>{
     setFormData((prevState)=>({
@@ -22,9 +48,24 @@ function Register() {
 
   const onSubmit=(e)=>{
     e.preventDefault();
+
+    if(password!==password2){
+      toast.error('PASSWORD DID NOT MATCH!')
+    }else{
+      const userData={
+        name,
+        email,
+        password,
+        role
+      }
+
+      dispatch(register(userData));
+    }
   }
 
-
+  if(isLoading){
+    return <Spinner/>
+  }
   return (<>
   <section className='heading'>
     <h1>
